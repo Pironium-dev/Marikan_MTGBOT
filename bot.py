4469d77f02d1bb0231955ddf0b7f748af4a682b0
piroping <pirogurama@gmail.com>
2023-12-17 14:00:42 +0900

    エラーメッセージの表示、通知されないバグの解決

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
 import asyncio
+import traceback
+import sys
 from config import config
 from datetime import datetime, timezone, timedelta
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,37 +23,47 @@ event_delete: dict[tuple[int, int], asyncio.events.TimerHandle] = {}
 
 
 def alarm():
-    a = asyncio.get_running_loop()
-    t = datetime.now(tz)
+    channel = client.get_channel(WORKING_CHANNEL)
     try:
-        name = event_name[(t.month, t.day)] + " "
-    except KeyError:
-        asyncio.run_coroutine_threadsafe(channel.send("名前がわからないMTGがあります。"), a)
-    asyncio.run_coroutine_threadsafe(channel.send(name + "のMTG開いてください"), a)
-    del event_name[(t.month, t.day)]
-    del event_delete[(t.month, t.day)]
+        a = asyncio.get_running_loop()
+        t = datetime.now(tz)
+        try:
+            name = event_name[(t.month, t.day)] + " "
+        except KeyError:
+            asyncio.run_coroutine_threadsafe(channel.send("名前がわからないMTGがあります。"), a)
+        asyncio.run_coroutine_threadsafe(channel.send(name + "のMTG開いてください"), a)
+        del event_name[(t.month, t.day)]
+        del event_delete[(t.month, t.day)]
+    except Exception as e:
+        asyncio.run(channel.send("".join(traceback.format_exception(*sys.exc_info()))))
 
 
 async def set_alarm(month, day, name):
-    t = datetime.now(tz)
-    target = t.replace(
-        month=month,
-        day=day,
-        hour=NOTIFI_TIME[0],
-        minute=NOTIFI_TIME[1],
-        second=0,
-        microsecond=0,
-    )
-    if target < t:
-        target = target.replace(year=target.year + 1)
-        global message
-        await message.channel.send('来年の')
-    a = asyncio.get_running_loop()
-    if (month, day) in event_delete:
-        event_name[(month, day)] += '&' + name
-    else:
-        event_name[(month, day)] = name
-        event_delete[(month, day)] = a.call_later((target - t).total_seconds(), alarm)
+    try:
+        t = datetime.now(tz)
+        target = t.replace(
+            month=month,
+            day=day,
+            hour=NOTIFI_TIME[0],
+            minute=NOTIFI_TIME[1],
+            second=0,
+            microsecond=0,
+        )
+        if target < t:
+            target = target.replace(year=target.year + 1)
+            channel = client.get_channel(WORKING_CHANNEL)
+            await channel.send("来年の")
+        a = asyncio.get_running_loop()
+        if (month, day) in event_delete:
+            event_name[(month, day)] += "&" + name
+        else:
+            event_name[(month, day)] = name
+            event_delete[(month, day)] = a.call_later(
+                (target - t).total_seconds(), alarm
+            )
+    except Exception as e:
+        channel = client.get_channel(WORKING_CHANNEL)
+        await channel.send("".join(traceback.format_exception(*sys.exc_info())))
 
 
 @client.event
@@ -62,64 +75,68 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
-    global tz
-    if message.author == client.user:
-        return
-    if message.channel.id != WORKING_CHANNEL:
-        return
-
-    order = message.content.split()
-    if order[0] == "mbot":
-        if len(order) == 1:
-            await message.channel.send("I'm ready.")
+    try:
+        global tz
+        if message.author == client.user:
             return
-        try:
-            if order[1] == "notify":
-                c = order[2].split("/")
-                name = order[3]
-                await message.channel.send(f"{c[0]}月{c[1]}日にお伝えします")
-                await set_alarm(int(c[0]), int(c[1]), name)
-            elif order[1] == "mtg":
-                c = order[2].split("/")
-                target = datetime.now(tz).replace(month=int(c[0]), day=int(c[1]))
-                name = order[3] + " "
-                l = [70, 40, 10]
-                if len(order[4:]) != 0:
-                    l = list(map(int, order[4:]))
-                for i in l:
-                    t = target - timedelta(days=i)
-                    await set_alarm(t.month, t.day, name)
-                    await message.channel.send(f"{t.month}月{t.day}日")
-                await message.channel.send("にお知らせします。")
-            elif order[1] == "del":
-                c = order[2].split("/")
-                n = (int(c[0]), int(c[1]))
-                try:
-                    event_delete[(n[0], n[1])].cancel()
-                except KeyError:
-                    await message.channel.send("存在しない日付です")
+        if message.channel.id != WORKING_CHANNEL:
+            return
+
+        order = message.content.split()
+        if order[0] == "mbot":
+            if len(order) == 1:
+                await message.channel.send("I'm ready.")
+                return
+            try:
+                if order[1] == "notify":
+                    c = order[2].split("/")
+                    if len(order) == 4:
+                        name = order[3]
+                    else:
+                        name = "ミーティング"
+                    await set_alarm(int(c[0]), int(c[1]), name)
+                    await message.channel.send(f"{c[0]}月{c[1]}日にお伝えします")
+                elif order[1] == "mtg":
+                    c = order[2].split("/")
+                    target = datetime.now(tz).replace(month=int(c[0]), day=int(c[1]))
+                    name = order[3] + " "
+                    l = [70, 40, 10]
+                    if len(order[4:]) != 0:
+                        l = list(map(int, order[4:]))
+                    for i in l:
+                        t = target - timedelta(days=i)
+                        await set_alarm(t.month, t.day, name)
+                        await message.channel.send(f"{t.month}月{t.day}日")
+                    await message.channel.send("にお知らせします。")
+                elif order[1] == "del":
+                    c = order[2].split("/")
+                    n = (int(c[0]), int(c[1]))
+                    try:
+                        event_delete[(n[0], n[1])].cancel()
+                    except KeyError:
+                        await message.channel.send("存在しない日付です")
+                    else:
+                        await message.channel.send(f"{event_name[(n[0], n[1])]}を削除しました")
+                        del event_delete[(n[0], n[1])]
+                        del event_name[(n[0], n[1])]
+                elif order[1] == "show":
+                    await message.channel.send(
+                        f"{NOTIFI_TIME[0]}時{NOTIFI_TIME[1]}分にお知らせします"
+                    )
+                    for (a, b), j in event_name.items():
+                        await message.channel.send(f"{a}/{b} {j}")
+                elif order[1] == "time":
+                    await message.channel.send(str(datetime.now(tz)))
                 else:
-                    await message.channel.send(f"{event_name[(n[0], n[1])]}を削除しました")
-                    del event_delete[(n[0], n[1])]
-                    del event_name[(n[0], n[1])]
-            elif order[1] == "show":
-                await message.channel.send(
-                    f"{NOTIFI_TIME[0]}時{NOTIFI_TIME[1]}分にお知らせします"
-                )
-                for (a, b), j in event_name.items():
-                    await message.channel.send(f"{a}/{b} {j}")
-            elif order[1] == "time":
-                await message.channel.send(
-                    str(datetime.now(tz))
-                )
-            else:
-                await message.channel.send("認識されないコマンドが入力されました")
-        except IndexError as e:
-            await message.channel.send("必要な引数が入力されてません")
-            raise e
-        except ValueError as e:
-            await message.channel.send("不正な引数が入力されました")
-            raise e
+                    await message.channel.send("認識されないコマンドが入力されました")
+            except IndexError as e:
+                await message.channel.send("必要な引数が入力されてません")
+                raise e
+            except ValueError as e:
+                await message.channel.send("不正な引数が入力されました")
+                raise e
+    except Exception as e:
+        await message.channel.send("".join(traceback.format_exception(*sys.exc_info())))
 
 
 if __name__ == "__main__":
